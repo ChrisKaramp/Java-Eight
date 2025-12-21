a868fe7d7f2dfa3e1f6fcbd8a9fd75b9c3ea0bd8
Ioannis Gongakis <t8240317@aueb.gr>
2025-12-21 20:59:28 +0200

    Delete target/class files
    They should't be here, we want a clean maven
    structure.

--- Listening_button.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JButton;
-
-public class Listening_button extends JButton {
-  
-      public Listening_button(String button_caption, String button_type) {
-        this.setText(button_caption);
-        addActionListener(new Button_handler(button_type));
-      }
-}
